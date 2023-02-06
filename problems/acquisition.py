41bebbd9b8ba09a0d2199772294f0a8c422b400e
pierre <pierre.osselin@gmail.com>
2023-02-06 16:24:34 +0000

    Implement pi bo and random

---
+++ problems/acquisition.py
@@ -0,0 +1,71 @@
+from botorch.acquisition.monte_carlo import qExpectedImprovement
+from typing import Any, Optional, Union
+from botorch.models.model import Model
+from torch.distributions.distribution import Distribution
+from torch import Tensor
+import torch
+from botorch.acquisition.objective import (
+    MCAcquisitionObjective,
+    PosteriorTransform,
+)
+from botorch.models.model import Model
+from botorch.sampling.base import MCSampler
+from torch import Tensor
+from botorch.utils.transforms import (
+    concatenate_pending_points,
+    t_batch_mode_transform,
+)
+
+
+class piqExpectedImprovement(qExpectedImprovement):
+    def __init__(
+        self,
+        model: Model,
+        best_f: Union[float, Tensor],
+        pi_distrib: Distribution,
+        n_iter: int,
+        beta: float,
+        sampler: Optional[MCSampler] = None,
+        objective: Optional[MCAcquisitionObjective] = None,
+        posterior_transform: Optional[PosteriorTransform] = None,
+        X_pending: Optional[Tensor] = None,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            model=model,
+            best_f=best_f,
+            sampler=sampler,
+            objective=objective,
+            posterior_transform=posterior_transform,
+            X_pending=X_pending,
+        )
+        self.register_buffer("best_f", torch.as_tensor(best_f, dtype=float))
+        self.pi_dist = pi_distrib
+        self.n_iter = n_iter
+        self.beta = beta
+
+    @concatenate_pending_points
+    @t_batch_mode_transform()
+    def forward(self, X: Tensor) -> Tensor:
+
+        r"""Evaluate qExpectedImprovement on the candidate set `X` with pi-BO weighting
+
+        Args:
+            X: A `batch_shape x q x d`-dim Tensor of t-batches with `q` `d`-dim design
+                points each.
+
+        Returns:
+            A `batch_shape'`-dim Tensor of Expected Improvement values at the given
+            design points `X`, where `batch_shape'` is the broadcasted batch shape of
+            model and input `X`.
+        """
+
+        posterior = self.model.posterior(
+            X=X, posterior_transform=self.posterior_transform
+        )
+        samples = self.get_posterior_samples(posterior)
+        obj = self.objective(samples, X=X)
+        obj = (obj - self.best_f.unsqueeze(-1).to(obj)).clamp_min(0)
+        q_ei = obj.max(dim=-1)[0].mean(dim=0)
+        pi_X = (self.beta / self.n_iter)*self.pi_dist.log_prob(X).sum(axis = 1)
+        return q_ei * torch.exp(pi_X)
--- problems/bo.py
+++ problems/bo.py
@@ -8,6 +8,8 @@ from botorch import fit_gpytorch_mll
 from botorch.acquisition.monte_carlo import qExpectedImprovement
 from botorch.sampling.normal import SobolQMCNormalSampler
 from botorch.exceptions import BadInitialCandidatesWarning
+from .acquisition import piqExpectedImprovement
+from torch.distributions.multivariate_normal import MultivariateNormal
 import time
 import warnings
 import matplotlib.pyplot as plt
@@ -35,9 +37,14 @@ def run():
     N_BATCH = 20 if not SMOKE_TEST else 2
     MC_SAMPLES = 256 if not SMOKE_TEST else 32
 
+    #Pi bo parameters
+    BETA = 0.3
+    mean, loc = torch.zeros(2, device=device, dtype=dtype), torch.eye(2, device=device, dtype=dtype)
+    pi_distrib = MultivariateNormal(mean, loc)
+
     verbose = False
 
-    best_observed_all_ei, best_observed_all_nei, best_random_all = [], [], []
+    best_observed_all_ei, best_observed_all_pi, best_random_all = [], [], []
 
     def generate_initial_data(n=10):
         # generate training data
@@ -74,19 +81,34 @@ def run():
         exact_obj = objective(new_x).unsqueeze(-1) # add output dimension
         new_obj = exact_obj + NOISE_SE * torch.randn_like(exact_obj)
         return new_x, new_obj
+    
+    def update_random_observations(best_random):
+        """Simulates a random policy by taking a the current list of best values observed randomly,
+        drawing a new random point, observing its value, and updating the list.
+        """
+        rand_x = torch.rand(BATCH_SIZE, 2)
+        next_random_best = objective(rand_x).max().item()
+        best_random.append(max(best_random[-1], next_random_best))       
+        return best_random
 
 
     # average over multiple trials
     for trial in range(1, N_TRIALS + 1):
         
         print(f"\nTrial {trial:>2} of {N_TRIALS} ", end="")
-        best_observed_ei, best_observed_nei, best_random = [], [], []
+        best_observed_ei, best_observed_pi, best_random = [], [], []
         
         # call helper functions to generate initial training data and initialize model
         train_x_ei, train_obj_ei, best_observed_value_ei = generate_initial_data(n=10)
         mll_ei, model_ei = initialize_model(train_x_ei, train_obj_ei)
-                
+
+        train_x_pi, train_obj_pi = train_x_ei, train_obj_ei
+        best_observed_value_pi = best_observed_value_ei
+        mll_pi, model_pi = initialize_model(train_x_pi, train_obj_pi)
+
         best_observed_ei.append(best_observed_value_ei)
+        best_observed_pi.append(best_observed_value_pi)
+        best_random.append(best_observed_value_ei)
         
         # run N_BATCH rounds of BayesOpt after the initial random batch
         for iteration in range(1, N_BATCH + 1):    
@@ -95,6 +117,7 @@ def run():
             
             # fit the models
             fit_gpytorch_mll(mll_ei)
+            fit_gpytorch_mll(mll_pi)
             
             # define the qEI and qNEI acquisition modules using a QMC sampler
             qmc_sampler = SobolQMCNormalSampler(sample_shape=torch.Size([MC_SAMPLES]))
@@ -106,16 +129,35 @@ def run():
                 sampler=qmc_sampler
             )
             
+            qPI = piqExpectedImprovement(
+                model=model_pi,
+                best_f=train_obj_pi.max(),
+                pi_distrib=pi_distrib,
+                n_iter=iteration,
+                beta=BETA,
+                sampler=qmc_sampler
+            )
+
             # optimize and get new observation
             new_x_ei, new_obj_ei = optimize_acqf_and_get_observation(qEI)
+            new_x_pi, new_obj_pi = optimize_acqf_and_get_observation(qPI)
+
                     
             # update training points
             train_x_ei = torch.cat([train_x_ei, new_x_ei])
             train_obj_ei = torch.cat([train_obj_ei, new_obj_ei])
 
+            train_x_pi = torch.cat([train_x_pi, new_x_pi])
+            train_obj_pi = torch.cat([train_obj_pi, new_obj_pi])
+
             # update progress
+            best_random = update_random_observations(best_random)
+
             best_value_ei = train_obj_ei.max().item()
             best_observed_ei.append(best_value_ei)
+
+            best_value_pi = train_obj_pi.max().item()
+            best_observed_pi.append(best_value_pi)
 
             # reinitialize the models so they are ready for fitting on next iteration
             # use the current state dict to speed up fitting
@@ -124,20 +166,25 @@ def run():
                 train_obj_ei, 
                 model_ei.state_dict(),
             )
+
+            mll_pi, model_pi = initialize_model(
+                train_x_pi, 
+                train_obj_pi,  
+                model_pi.state_dict(),
+            )
             
             t1 = time.monotonic()
             
             if verbose:
                 print(
                     f"\nBatch {iteration:>2}: best_value (random, qEI, qNEI) = "
-                    f"({max(best_random):>4.2f}, {best_value_ei:>4.2f}, {best_value_nei:>4.2f}), "
                     f"time = {t1-t0:>4.2f}.", end=""
                 )
             else:
                 print(".", end="")
     
         best_observed_all_ei.append(best_observed_ei)
-        best_observed_all_nei.append(best_observed_nei)
+        best_observed_all_pi.append(best_observed_pi)
         best_random_all.append(best_random)
     
     def ci(y):
@@ -147,11 +194,15 @@ def run():
 
     iters = np.arange(N_BATCH + 1) * BATCH_SIZE
     y_ei = np.asarray(best_observed_all_ei)
+    y_pi = np.asarray(best_observed_all_pi)
+    y_rnd = np.asarray(best_random_all)
+
 
     _, ax = plt.subplots(1, 1, figsize=(8, 6))
     ax.errorbar(iters, y_ei.mean(axis=0), yerr=ci(y_ei), label="qEI", linewidth=1.5)
+    ax.errorbar(iters, y_pi.mean(axis=0), yerr=ci(y_pi), label="pi_qEI", linewidth=1.5)
+    ax.errorbar(iters, y_rnd.mean(axis=0), yerr=ci(y_rnd), label="random", linewidth=1.5)
     ax.plot([0, N_BATCH * BATCH_SIZE], [GLOBAL_MAXIMUM] * 2, 'k', label="true best objective", linewidth=2)
-    #ax.set_ylim(bottom=0.5)
     ax.set(xlabel='number of observations (beyond initial points)', ylabel='best objective value')
     ax.legend(loc="lower right")
 
