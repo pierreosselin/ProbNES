Fix memory leakage

module/BASQ/_rchq.py
@@ -27,14 +27,16 @@ def recombination(
 
 
 def ker_svd_sparsify(pt, s, kernel):
-    _U, S, _ = torch.svd_lowrank(kernel(pt, pt), q=s)
+    with torch.no_grad():
+        _U, S, _ = torch.svd_lowrank(kernel(pt, pt), q=s)
     U = -1 * _U.T  # Hermitian
     return S, U
 
 
 def rc_kernel_svd(samp, pt, s, kernel, device, dtype, mu=0, use_obj=True):
     # Nystrom method
-    _, U = ker_svd_sparsify(pt, s - 1, kernel)
+    with torch.no_grad():
+        _, U = ker_svd_sparsify(pt, s - 1, kernel)
     w_star, idx_star = Mod_Tchernychova_Lyons(
         samp, U, pt, kernel, device, dtype, mu, use_obj=use_obj
     )
@@ -56,79 +58,80 @@ def Mod_Tchernychova_Lyons(samp, U_svd, pt_nys, kernel, device, dtype, mu=0, use
     idx_story = torch.arange(N).to(device)
     idx_story = idx_story[mu != 0]
     remaining_points = len(idx_story)
+    with torch.no_grad():
+        while True:
+            if remaining_points <= n + 1:
+                idx_star = torch.arange(len(mu), device=device)[mu > 0].to(device)
+                w_star = mu[idx_star]
+                return w_star, idx_star
 
-    while True:
-        if remaining_points <= n + 1:
-            idx_star = torch.arange(len(mu), device=device)[mu > 0].to(device)
-            w_star = mu[idx_star]
-            return w_star, idx_star
+            elif n + 1 < remaining_points <= number_of_sets:
+                X_mat = U_svd @ kernel(pt_nys, samp[idx_story])
+                w_star, idx_star, x_star, _, ERR, _, _ = Tchernychova_Lyons_CAR(
+                    X_mat.T, torch.clone(mu[idx_story]), device, dtype, DEBUG)
+                idx_story = idx_story[idx_star]
+                mu[:] = 0.
+                mu[idx_story] = w_star
+                idx_star = idx_story
+                w_star = mu[mu > 0]
+                return w_star, idx_star
 
-        elif n + 1 < remaining_points <= number_of_sets:
-            X_mat = U_svd @ kernel(pt_nys, samp[idx_story])
-            w_star, idx_star, x_star, _, ERR, _, _ = Tchernychova_Lyons_CAR(
-                X_mat.T, torch.clone(mu[idx_story]), device, dtype, DEBUG)
-            idx_story = idx_story[idx_star]
-            mu[:] = 0.
-            mu[idx_story] = w_star
-            idx_star = idx_story
-            w_star = mu[mu > 0]
-            return w_star, idx_star
+            number_of_el = int(remaining_points / number_of_sets)
 
-        number_of_el = int(remaining_points / number_of_sets)
+            idx = idx_story[:number_of_el * number_of_sets].reshape(number_of_el, -1)
+            X_for_nys = torch.zeros((length, number_of_sets)).to(device, dtype)
+            # X_for_obj = torch.zeros((1, number_of_sets)).to(device)
+            for i in range(number_of_el):
+                idx_tmp_i = idx_story[i * number_of_sets:(i + 1) * number_of_sets]
+                
+                X_for_nys += torch.multiply(
+                    kernel(pt_nys, samp[idx_tmp_i]),
+                    mu[idx_tmp_i].unsqueeze(0)
+                )
 
-        idx = idx_story[:number_of_el * number_of_sets].reshape(number_of_el, -1)
-        X_for_nys = torch.zeros((length, number_of_sets)).to(device, dtype)
-        # X_for_obj = torch.zeros((1, number_of_sets)).to(device)
-        for i in range(number_of_el):
-            idx_tmp_i = idx_story[i * number_of_sets:(i + 1) * number_of_sets]
-            X_for_nys += torch.multiply(
-                kernel(pt_nys, samp[idx_tmp_i]),
-                mu[idx_tmp_i].unsqueeze(0)
+            X_tmp_tr = U_svd @ X_for_nys
+            X_tmp = X_tmp_tr.T
+            tot_weights = torch.sum(mu[idx], 0).to(device)
+            idx_last_part = idx_story[number_of_el * number_of_sets:]
+
+            if len(idx_last_part):
+                X_mat = U_svd @ kernel(pt_nys, samp[idx_last_part])
+                X_tmp[-1] += torch.multiply(
+                    X_mat.T,
+                    mu[idx_last_part].unsqueeze(1)
+                ).sum(axis=0)
+                tot_weights[-1] += torch.sum(mu[idx_last_part], 0)
+
+            X_tmp = torch.divide(X_tmp, tot_weights.unsqueeze(0).T)
+
+            w_star, idx_star, _, _, ERR, _, _ = Tchernychova_Lyons_CAR(
+                X_tmp, torch.clone(tot_weights), device, dtype
             )
 
-        X_tmp_tr = U_svd @ X_for_nys
-        X_tmp = X_tmp_tr.T
-        tot_weights = torch.sum(mu[idx], 0).to(device)
-        idx_last_part = idx_story[number_of_el * number_of_sets:]
+            idx_tomaintain = idx[:, idx_star].reshape(-1)
+            idx_tocancel = torch.ones(idx.shape[1]).to(torch.bool).to(device)
+            idx_tocancel[idx_star] = 0
+            idx_tocancel = idx[:, idx_tocancel].reshape(-1)
 
-        if len(idx_last_part):
-            X_mat = U_svd @ kernel(pt_nys, samp[idx_last_part])
-            X_tmp[-1] += torch.multiply(
-                X_mat.T,
-                mu[idx_last_part].unsqueeze(1)
-            ).sum(axis=0)
-            tot_weights[-1] += torch.sum(mu[idx_last_part], 0)
+            mu[idx_tocancel] = 0.
+            mu_tmp = torch.multiply(mu[idx[:, idx_star]], w_star)
+            mu_tmp = torch.divide(mu_tmp, tot_weights[idx_star])
+            mu[idx_tomaintain] = mu_tmp.reshape(-1)
 
-        X_tmp = torch.divide(X_tmp, tot_weights.unsqueeze(0).T)
+            idx_tmp = idx_star == number_of_sets - 1
+            idx_tmp = torch.arange(len(idx_tmp), device=device)[idx_tmp != 0].to(device)
+            # if idx_star contains the last barycenter, whose set could have more points
+            if len(idx_tmp) > 0:
+                mu_tmp = torch.multiply(mu[idx_last_part], w_star[idx_tmp])
+                mu_tmp = torch.divide(mu_tmp, tot_weights[idx_star[idx_tmp]])
+                mu[idx_last_part] = mu_tmp
+                idx_tomaintain = torch.cat([idx_tomaintain, idx_last_part])
+            else:
+                idx_tocancel = torch.cat([idx_tocancel, idx_last_part])
+                mu[idx_last_part] = 0.
 
-        w_star, idx_star, _, _, ERR, _, _ = Tchernychova_Lyons_CAR(
-            X_tmp, torch.clone(tot_weights), device, dtype
-        )
-
-        idx_tomaintain = idx[:, idx_star].reshape(-1)
-        idx_tocancel = torch.ones(idx.shape[1]).to(torch.bool).to(device)
-        idx_tocancel[idx_star] = 0
-        idx_tocancel = idx[:, idx_tocancel].reshape(-1)
-
-        mu[idx_tocancel] = 0.
-        mu_tmp = torch.multiply(mu[idx[:, idx_star]], w_star)
-        mu_tmp = torch.divide(mu_tmp, tot_weights[idx_star])
-        mu[idx_tomaintain] = mu_tmp.reshape(-1)
-
-        idx_tmp = idx_star == number_of_sets - 1
-        idx_tmp = torch.arange(len(idx_tmp), device=device)[idx_tmp != 0].to(device)
-        # if idx_star contains the last barycenter, whose set could have more points
-        if len(idx_tmp) > 0:
-            mu_tmp = torch.multiply(mu[idx_last_part], w_star[idx_tmp])
-            mu_tmp = torch.divide(mu_tmp, tot_weights[idx_star[idx_tmp]])
-            mu[idx_last_part] = mu_tmp
-            idx_tomaintain = torch.cat([idx_tomaintain, idx_last_part])
-        else:
-            idx_tocancel = torch.cat([idx_tocancel, idx_last_part])
-            mu[idx_last_part] = 0.
-
-        idx_story = torch.clone(idx_tomaintain)
-        remaining_points = len(idx_story)
+            idx_story = torch.clone(idx_tomaintain)
+            remaining_points = len(idx_story)
 
 
 def Tchernychova_Lyons_CAR(X, mu, device, dtype, DEBUG=False):

module/bo.py
@@ -18,6 +18,8 @@ from tqdm import tqdm
 import numpy as np
 import random
 from botorch.utils.transforms import standardize, normalize, unnormalize
+import gc
+
 
 
 from evotorch.algorithms import SNES
@@ -91,7 +93,6 @@ def run(save_path: str,
     verbose = True
 
     #train_yvar = torch.tensor(objective.noise_std**2, device=device, dtype=dtype)
-    # TODO Normalize data for gp estimation
     def initialize_model(train_x, train_obj, state_dict=None):
         # define models for objective and constraint
         model_obj = SingleTaskGP(train_x, train_obj).to(train_x)
@@ -160,9 +161,16 @@ def run(save_path: str,
     if label in ["qEI", "piqEI", "random"]:
         # call helper functions to generate initial training data and initialize model
         train_x, train_obj, best_observed_value = problem.generate_initial_data(n=10)
-    elif label in ["SNES", "NESWSABI"]:
+    elif label == "SNES":
         searcher.run(1)
         train_x, train_obj = searcher.population.values, searcher.population.evals
+        best_observed_value = train_obj.max().item()
+        list_mu.append(searcher._get_mu())
+        list_sigma.append(searcher._get_sigma())
+    
+    elif label == "NESWSABI":
+        searcher.run(1, train_x=torch.tensor([], device=device, dtype=dtype), train_y=torch.tensor([], device=device, dtype=dtype))
+        train_x, train_obj = searcher.population.values.detach().clone(), searcher.population.evals.detach().clone().flatten()
         best_observed_value = train_obj.max().item()
         list_mu.append(searcher._get_mu())
         list_sigma.append(searcher._get_sigma())
@@ -194,7 +202,7 @@ def run(save_path: str,
                     best_f=standardize(train_obj).max(),
                     sampler=qmc_sampler
                 )
-            if label == "piqEI":                
+            if label == "piqEI":
                 af = piqExpectedImprovement(
                     model=model,
                     best_f=standardize(train_obj).max(),
@@ -215,9 +223,15 @@ def run(save_path: str,
                 new_x, new_obj = update_random_observations()
             elif problem_name == "airfoil":
                 new_x, new_obj = update_random_observations_discrete()
-        elif label in ["SNES", "NESWSABI"]:
+        elif label == "SNES":
             searcher.run(1)
             new_x, new_obj = searcher.population.values, searcher.population.evals
+            list_mu.append(searcher._get_mu())
+            list_sigma.append(searcher._get_sigma())
+        elif label == "NESWSABI":
+            #searcher = NESWSABI(problem_ea, popsize=BATCH_SIZE, stdev_init=problem_kwargs["initial_bounds"], ranking_method=bo_kwargs["ranking_method"], quad_kwargs=bo_kwargs["quadrature"])
+            searcher.run(1, train_x=train_x, train_y=train_obj)
+            new_x, new_obj = searcher.population.values.detach().clone(), searcher.population.evals.detach().clone().flatten()
             list_mu.append(searcher._get_mu())
             list_sigma.append(searcher._get_sigma())
 
@@ -239,7 +253,7 @@ def run(save_path: str,
             )
         
         t1 = time.monotonic()
-        
+
         if verbose:
             if (iteration + 1)%10 == 0:
                 print(

module/search.py
@@ -11,10 +11,10 @@ from evotorch.distributions import (
     SeparableGaussian,
     SymmetricSeparableGaussian,
 )
+from datetime import datetime
 import torch
 from torch.distributions import MultivariateNormal
 from .BASQ._basq_search import BASQ
-# TODO Data currently saved both in BO and in search
 # TODO Initial distribution should be mu = 0 for fairer comparison
 
 class NESWSABI(SNES):
@@ -69,12 +69,50 @@ class NESWSABI(SNES):
                 )
         # We kee track of the population sampled
         self.quad_kwargs = quad_kwargs
-        self.train_x = torch.tensor([], device = self.problem.device, dtype = self.problem.dtype)
-        self.train_y = torch.tensor([], device = self.problem.device, dtype = self.problem.dtype)
         self.weights = torch.tensor([], device = self.problem.device, dtype = self.problem.dtype)
         self._distribution.mu = torch.zeros(self.problem.solution_length, device = self.problem.device, dtype = self.problem.dtype)
+    
+    def step(self, train_x:torch.Tensor, train_y:torch.Tensor):
+        """
+        Perform a step of the search algorithm.
+        """
+        self._before_step_hook()
+        self.clear_status()
 
-    def _step_non_distributed(self):
+        if self._first_step_datetime is None:
+            self._first_step_datetime = datetime.now()
+
+        self._step(train_x, train_y)
+        self._steps_count += 1
+        self.update_status({"iter": self._steps_count})
+        self.update_status(self._problem.status)
+        extra_status = self._after_step_hook.accumulate_dict()
+        self.update_status(extra_status)
+        if len(self._log_hook) >= 1:
+            self._log_hook(dict(self.status))
+
+    def run(self, num_generations: int, train_x:torch.Tensor, train_y:torch.Tensor, *, reset_first_step_datetime: bool = True):
+        """
+        Run the algorithm for the given number of generations
+        (i.e. iterations).
+        Args:
+            num_generations: Number of generations.
+            reset_first_step_datetime: If this argument is given as True,
+                then, the datetime of the first search step will be forgotten.
+                Forgetting the first step's datetime means that the first step
+                taken by this new run will be the new first step datetime.
+        """
+        if reset_first_step_datetime:
+            self.reset_first_step_datetime()
+
+        for _ in range(int(num_generations)):
+            self.step(train_x, train_y)
+
+        if len(self._end_of_run_hook) >= 1:
+            self._end_of_run_hook(dict(self.status))
+
+
+    def _step_non_distributed(self, train_x, train_y):
         # First, we define an inner function which fills the current population by sampling from the distribution.
         def fill_and_eval_pop():
             # This inner function is responsible for filling the main population with samples
@@ -95,8 +133,8 @@ class NESWSABI(SNES):
                     prior = MultivariateNormal(self._distribution.mu, torch.diag(self._distribution.sigma))
                     true_likelihood, device, dtype = self.problem._objective_func, self.problem.device, self.problem.dtype
                     self.quad = BASQ(
-                        self.train_x,  # initial locations
-                        standardize(self.train_y),  # initial observations
+                        train_x,  # initial locations
+                        standardize(train_y),  # initial observations
                         prior,  # Gaussian prior distribution
                         true_likelihood,  # true likelihood to be estimated
                         device,  # cpu or cuda
@@ -106,7 +144,6 @@ class NESWSABI(SNES):
                     # self._population.access_values() to change to quadrature point selection
                     result = self.quad.run(1)
                     self._population.set_values(result[0][0])
-                    #self._population.set_evals(result[0][1])
                     
                 else:
                     self._distribution.sample(out=self._population.access_values(), generator=self.problem)
@@ -117,8 +154,6 @@ class NESWSABI(SNES):
                 #Get log prob of values for importance sampling
                 m = MultivariateNormal(self._get_mu(), torch.diag(self._get_sigma()))
                 # Save data
-                self.train_x = torch.cat([self.train_x, self._population.values.detach().clone()])
-                self.train_y = torch.cat([self.train_y, self._population.evals.detach().clone().flatten()])
                 self.weights = torch.cat([self.weights, m.log_prob(self.population.values.detach().clone())])
                 
             else:
@@ -189,11 +224,11 @@ class NESWSABI(SNES):
             # If we are computing next generations, then we need to compute the gradients of the last
             # generation, sample a new population, and evaluate the new population's solutions.
             #samples = self._population.access_values(keep_evals=True)
-            samples = self.train_x
+            samples = train_x
             
             #fitnesses = self._population.access_evals()[:, self._obj_index]
             m = MultivariateNormal(self._get_mu(), torch.diag(self._get_sigma()))
-            fitnesses = self.train_y * torch.exp(m.log_prob(samples) - self.weights)
+            fitnesses = train_y * torch.exp(m.log_prob(samples) - self.weights)
             
             obj_sense = self.problem.senses[self._obj_index]
             ranking_method = self._ranking_method